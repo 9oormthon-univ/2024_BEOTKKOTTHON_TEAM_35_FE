213429e8d0168f69c93505579543e89587a02f13
seiyoon <sera4084@naver.com>
2024-03-14 02:41:29 +0900

    feat: 초기 세팅

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "돈버니",
+  description: "대학생을 위한 금융 학습 서비스",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}

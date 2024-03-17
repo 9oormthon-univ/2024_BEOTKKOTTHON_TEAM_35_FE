bd59c11de6785c6c9146cf0977caeb580b669109
krdevdory <krdevmon@gmail.com>
2024-03-17 18:11:47 +0900

    feat: 홈화면 탭 추가

---
+++ src/app/_components/home/TabsComponent.jsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { useState } from 'react';
+
+const TabsComponent = ({ items }) => {
+const [selectedTab, setSelectedTab] = useState(0);
+
+    return (
+        <div>
+            {/* 탭 헤더 */}
+            <div class="flex justify-between items-center h-[44px] px-[16px]">
+                {items.map((item, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setSelectedTab(index)}
+                        class={`font-bold ${selectedTab === index ? 'text-[#2528AE] underline underline-offset-8' : ''}`}
+                    >
+                        {item.title}
+                    </button>
+                ))}
+            </div>
+
+            {/* 탭 본문 */}
+            <div>
+                {items.map((item, index) => (
+                    <div className={`${selectedTab === index ? '' : 'hidden'}`}>
+                        {item.content}
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    );
+}
+
+export default TabsComponent;

3307a4dd195ed703609a190bc0d5f3b37604efd8
seiyoon <sera4084@naver.com>
2024-03-17 11:15:41 +0900

    feat: pwa 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({
+  // Your Next.js config
+});
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,38 @@ import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "돈버니";
+const APP_TITLE = "돈버니";
+const APP_DESCRIPTION = "대학생을 위한 금융 학습 서비스";
+
 export const metadata = {
-  title: "돈버니",
-  description: "대학생을 위한 금융 학습 서비스",
+  applicationName: APP_NAME,
+  title: APP_TITLE,
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: APP_TITLE,
+    description: APP_DESCRIPTION,
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body>{children}</body>
     </html>
   );

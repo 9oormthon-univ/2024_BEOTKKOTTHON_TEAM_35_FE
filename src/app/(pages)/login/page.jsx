3cf100f94c9a9e41e5a04a118fa572170d66c65b
seiyoon <sera4084@naver.com>
2024-03-18 15:59:19 +0900

    feat: 스플래시&첫화면 UI

---
+++ src/app/(pages)/login/page.jsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Login() {
+  return (
+    <div>
+      <div className="flex flex-col content-center items-center w-full h-full">
+        <div className="flex flex-col content-center items-center w-full max-w-[500px] h-screen bg-[#FCFCFF]">
+          로그인 화면
+        </div>
+      </div>
+    </div>
+  );
+}

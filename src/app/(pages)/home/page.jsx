aba21d0841a9c99a8bfbf82d8edce1665f5a6cad
krdevdory <krdevmon@gmail.com>
2024-03-20 11:26:33 +0900

    feat: 홈 UI 업데이트

--- src/app/(pages)/home/page.jsx
+++ src/app/(pages)/home/page.jsx
@@ -1,10 +1,10 @@
 import Header from "@/app/_components/home/Header";
 import BottomNav from "@/app/_components/bottom_nav";
+import HomeTab from "./home";
 import Tab1 from "./tab1";
 import Tab2 from "./tab2";
 import Tab3 from "./tab3";
 import Tab4 from "./tab4";
-import Tab5 from "./tab5";
 import TabsComponent from "@/app/_components/home/TabsComponent";
 
 export default function Home() {
@@ -14,7 +14,9 @@ export default function Home() {
         
         <Header />
 
-        <TabsComponent items={items}/>
+        {/* <TabsComponent items={items}/> */}
+
+        <HomeTab />
 
         <BottomNav />
       </div>
@@ -27,9 +29,8 @@ export default function Home() {
 
 // 탭 아이템
 const items = [
-  {title: "홈", content: (<Tab1 />)},
-  {title: "장학금", content: (<Tab2 />)},
-  {title: "청년정책", content: (<Tab3 />)},
-  {title: "금융상품", content: (<Tab4 />)},
-  {title: "챌린지", content: (<Tab5 />)},
+  {title: "장학금", content: (<Tab1 />)},
+  {title: "청년정책", content: (<Tab2 />)},
+  {title: "금융상품", content: (<Tab3 />)},
+  {title: "챌린지", content: (<Tab4 />)},
 ]

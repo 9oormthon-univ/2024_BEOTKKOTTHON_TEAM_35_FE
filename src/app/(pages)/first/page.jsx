2293ff0add031d5016efe8aa116627a53fa87020
seiyoon <sera@bzznbyd.com>
2024-03-22 22:04:20 +0900

    전화 테스트 복구

--- src/app/(pages)/first/page.jsx
+++ src/app/(pages)/first/page.jsx
@@ -1,20 +1,12 @@
 "use client";
 
-import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 import KakaoButton from "@/app/_components/common/KakaoButton";
 import BorderButton from "@/app/_components/common/BorderButton";
 
 export default function Login() {
-  const router = useRouter(); // 추가
-
-  function phoneCall(phoneNumber) {
-    location.href = "tel:" + phoneNumber; // num -> phoneNumber 수정
-  }
-
   return (
     <div>
       <div className="flex flex-col content-center items-center w-full h-full">
@@ -28,12 +20,12 @@ export default function Login() {
             />
           </div>
           <div className="absolute bottom-0 grid px-4 w-full gap-3.5 mb-14">
-            <KakaoButton onClick={phoneCall("01011112222")} /> {/* 수정 */}
-            <BorderButton
-              text="이메일 로그인"
-              onClick={() => router.push("/login")}
-            />{" "}
-            {/* 수정 */}
+            <Link href="/home">
+              <KakaoButton />
+            </Link>
+            <Link href="/login">
+              <BorderButton text="이메일 로그인" />
+            </Link>
           </div>
         </div>
       </div>

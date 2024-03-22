5f7873577146dba8b76f98b24a54e3754c79e1aa
seiyoon <sera@bzznbyd.com>
2024-03-22 09:05:12 +0900

    feat: bottom-nav에 퀴즈탭 추가

--- src/app/(pages)/quiz/page.jsx
+++ src/app/(pages)/quiz/page.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
+import BottomNav from "@/app/_components/bottom_nav";
 import TabsComponent from "@/app/_components/quiz/TabsComponent";
 import StudyTabContent from "@/app/_components/quiz/StudyTabContent";
 import ReviewTabContent from "@/app/_components/quiz/ReviewTabContent";
@@ -30,24 +31,27 @@ const items = [
 
 export default function Quiz() {
   return (
-    <div className="flex flex-col content-center items-center w-full h-full">
-      <div className="flex flex-col content-center items-center w-full max-w-[500px] h-screen bg-[#FCFCFF]">
-        <div className="fixed w-full bg-[#FCFCFF]">
-          <div className="mt-[28px] flex flex-col w-full justify-center items-center h-[36px] px-4">
-            <div className="text-[16px]">퀴즈</div>
+    <div className="w-full max-w-[500px] pb-14">
+      <div className="flex flex-col content-center items-center w-full h-full">
+        <div className="flex flex-col content-center items-center w-full max-w-[500px] h-screen bg-[#FCFCFF]">
+          <div className="fixed w-full bg-[#FCFCFF]">
+            <div className="mt-[28px] flex flex-col w-full justify-center items-center h-[36px] px-4">
+              <div className="text-[16px]">퀴즈</div>
+            </div>
           </div>
-        </div>
-        <div className="w-full mt-[44px]">
-          <TabsComponent items={items} />
-        </div>
-        {/* <div className="flex flex-col px-4 items-start w-full h-full">
-          <div className="mt-[21px] w-full rounded-xl bg-[#FCFCFF] shadow-md">
-            학습진도율
+          <div className="w-full mt-[44px]">
+            <TabsComponent items={items} />
           </div>
-          <div className="mt-[24px] text-[22px] font-bold">금융 퀴즈</div>
-          <div className="w-full mt-[19px]">{`카테고리 박스 4개`}</div>
-        </div> */}
+          {/* <div className="flex flex-col px-4 items-start w-full h-full">
+        <div className="mt-[21px] w-full rounded-xl bg-[#FCFCFF] shadow-md">
+          학습진도율
+        </div>
+        <div className="mt-[24px] text-[22px] font-bold">금융 퀴즈</div>
+        <div className="w-full mt-[19px]">{`카테고리 박스 4개`}</div>
+      </div> */}
+        </div>
       </div>
+      <BottomNav />
     </div>
   );
 }

c0325d3d261ac335286fecb22af9067df5fdd2c4
krdevdory <krdevmon@gmail.com>
2024-03-23 16:09:46 +0900

    feat: <Link /> to <a />

--- src/app/_components/bottom_nav.jsx
+++ src/app/_components/bottom_nav.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from 'react';
-import Link from 'next/link';
 import useNavigation from '@/hook/use-navigation';
 
 import Home from './bottom_images/bottom_home.svg'
@@ -30,14 +29,14 @@ const BottomNav = () => {
             <span class="mt-[5px] font-bold text-[#4E60FF] text-[10px] font-medium leading-4">홈</span>
           </div>
         ) : (
-          <Link href="/home" class="flex items-center relative">
+          <a href="/home" class="flex items-center relative">
             <div class="flex flex-col justify-center items-center">
               <div class="flex w-[23px] h-[23px] justify-center items-center">
                 <Home fill="#C9CDD2"/>
               </div>
               <span class="mt-[5px] text-[#9EA4AA] text-[10px] font-medium">홈</span>
             </div>
-          </Link>
+          </a>
         )}
 
         {isExploreActive ? (
@@ -48,14 +47,14 @@ const BottomNav = () => {
             <span class="mt-[5px] font-bold text-[#4E60FF] text-[10px] font-medium leading-4">탐색</span>
           </div>
         ) : (
-          <Link href="/explore" class="flex items-center relative">
+          <a href="/explore" class="flex items-center relative">
             <div class="flex flex-col justify-center items-center">
               <div class="flex w-[23px] h-[23px] justify-center items-center">
                 <Explore fill="#C9CDD2"/>
               </div>
               <span class="mt-[5px] text-[#9EA4AA] text-[10px] font-medium">탐색</span>
             </div>
-          </Link>
+          </a>
         )}
 
         {isQuizActive ? (
@@ -66,14 +65,14 @@ const BottomNav = () => {
             <span class="mt-[5px] font-bold text-[#4E60FF] text-[10px] font-medium leading-4">퀴즈</span>
           </div>
         ) : (
-          <Link href="/quiz" class="flex items-center">
+          <a href="/quiz" class="flex items-center">
             <div class="flex flex-col justify-center items-center">
               <div class="flex w-[23px] h-[23px] justify-center items-center">
                 <Quiz fill="#C9CDD2"/>
               </div>
               <span class="mt-[5px] text-[#9EA4AA] text-[10px] font-medium">퀴즈</span>
             </div>
-          </Link>
+          </a>
         )}
 
 
@@ -86,14 +85,14 @@ const BottomNav = () => {
             <span class="mt-[5px] font-bold text-[#4E60FF] text-[10px] font-medium leading-4">MY</span>
           </div>
         ) : (
-          <Link href="/mypage" class="flex items-center">
+          <a href="/mypage" class="flex items-center">
             <div class="flex flex-col justify-center items-center">
               <div class="flex w-[23px] h-[23px] justify-center items-center">
                 <My fill="#C9CDD2"/>
               </div>
               <span class="mt-[5px] text-[#9EA4AA] text-[10px] font-medium">MY</span>
             </div>
-          </Link>
+          </a>
         )}
       </div>
     </div>
